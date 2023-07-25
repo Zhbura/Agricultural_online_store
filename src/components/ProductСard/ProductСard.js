7fc19b8db761bd3a8864607cccb60b8bf8d3caed
Zhbura <zhbura.nadezhda@yandex.ru>
2023-07-25 11:59:42 +0300

    Исправление карточки товара, замена svg изображения

--- src/components/ProductСard/ProductСard.js
+++ src/components/ProductСard/ProductСard.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Cart } from '../SVG/Icon/Cart';
-import { Comparison } from '../SVG/Icon/Comparison';
 import './ProductСard.scss';
+import { Heart } from '../SVG/Icon/Heart';
 
 export const ProductCard = ({ product, addToCart }) => {
     const { id, name, alt, img, price, to } = product;
@@ -10,13 +10,14 @@ export const ProductCard = ({ product, addToCart }) => {
         <>
             <div className="product-card" key={id}>
                 <img className="product-card__main-img" src={img[0]} alt={alt} />
-                <div className="product-card__comparison"><Comparison /></div>
+                <div className="product-card__heart">
+                    <div className="circle-icon circle-icon_hover"><Heart /></div></div>
                 <div className="product-card__info-product">
                     <div className="product-card__text">
                         <p className="product-card__name">
                             <Link to={`/product/${to}`} className="product-card__link">{name}</Link>
                         </p>
-                        <p className="product-card__price">{price}</p>
+                        <p className="product-card__price">{price} руб.</p>
                         <p className="product-card__quantity">1 шт</p>
                     </div>
                     <div className="product-card__cart" onClick={() => addToCart(id, product)}>
